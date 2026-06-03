5daf78671bb4fb1190b73f159329d94bfcdb68c8
prade <prade@Pradeep_Krishna>
2026-06-03 23:42:34 +0530

    AllDOne

--- eclipsejune3/src/test/java/com/google/framework/tests/GoogleSearchTest.java
+++ eclipsejune3/src/test/java/com/google/framework/tests/GoogleSearchTest.java
@@ -18,6 +18,8 @@ public class GoogleSearchTest extends BaseTest {
         GoogleHomePage home = new GoogleHomePage(driver);
         home.search("Selenium WebDriver");
         
+        //Test
+        
         
      // 1. Create a wait object that will wait up to 10 seconds
         //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
